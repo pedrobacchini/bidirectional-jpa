71ced7f4b921b5c4169ef71f5b3f32953f193002
Pedro Bacchini <pedrobacchini@outlook.com>
2019-08-16 17:40:27 -0300

    Create project.

---
+++ src/test/java/com/github/pedrobacchini/bidirectionaljpa/BidirectionalJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.pedrobacchini.bidirectionaljpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BidirectionalJpaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
